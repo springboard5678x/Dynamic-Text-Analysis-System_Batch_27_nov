a232fb2dc72ad31dd0c90d1c238f677beebf2cac
DMHACKERZ <DEBRAJMISTRY17052002@GMAIL.COM>
2026-01-23 12:02:09 +0530

    Removing Unknowns

--- Dynamic-text-analysis-platform/download_semantic_model.py
+++
@@ -1,4 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-print("Semantic model downloaded successfully!")
--- Dynamic-text-analysis-platform/train_logistic.py
+++
@@ -1,5 +0,0 @@
-
-
-from src.logistic_sentiment import train_logistic_model
-
-train_logistic_model()
